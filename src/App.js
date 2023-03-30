c763b6251c40abb73a0f79774d4dd7871dce16f1
Wolrang <dkfud252@naver.com>
2023-03-30 19:33:56 +0900

    🎉 Wesely Project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import MapContainer from './component/MapContainer';
 import SearchPlacs from './component/SearchPlace';
 import Nav from './component/Nav';
+import Bottombar from './component/BottomBar'
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
       <SearchPlacs></SearchPlacs>
+      <Bottombar></Bottombar>
     </div>
   );
 }
